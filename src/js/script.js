7332acd4d493fca0fe3c96e61465c88e9fca9caf
PrinMax <max.prin@edu.univ-fcomte.fr>
2024-12-03 14:49:08 +0100

    code js

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,22 @@
-// *** LIGHTBOX ***
+const lightbox = document.querySelector("#lightbox");
+const lightboxImg = document.querySelector("#lightbox > img");
+
+document.body.addEventListener("click", (evt) => {
+    if (evt.target.matches("[data-full-img]")) {
+        lightboxImg.src = evt.target.dataset.fullImg;
+        lightbox.showModal();
+    }
+});
+
+lightbox.addEventListener("click", (evt) => {
+    lightbox.classList.add("sortie");
+    lightbox.addEventListener(
+        "animationend",
+        () => {
+            const lightbox = null;
+            lightbox.classList.remove("sortie");
+            lightbox.close();
+        },
+        { once: true }
+    );
+});
